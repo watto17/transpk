8a796ea514fbe69fc6b053d6f517fbe63bb8f51e
USMAN378 <usman.cheema7136@gmail.com>
2019-05-29 17:59:15 +0500

    sidebar options changes for now

--- client/src/modules/Dashboard/Sidebar/index.jsx
+++ client/src/modules/Dashboard/Sidebar/index.jsx
@@ -17,7 +17,7 @@ const sidebar=(props)=> {
   <div className="kt-aside-menu-wrapper kt-grid__item kt-grid__item--fluid" id="kt_aside_menu_wrapper">
     <div id="kt_aside_menu" className="kt-aside-menu kt-scroll ps" data-ktmenu-vertical={1} data-ktmenu-scroll={1} data-ktmenu-dropdown-timeout={500} style={{height: '1308px', overflow: 'hidden'}}>
       <ul className="kt-menu__nav ">
-        <li className={`kt-menu__item ${pathname === '/dashboard' ? 'kt-menu__item--active' : '' } `} aria-haspopup="true"><Link to='/dashboard'
+        <li className={`kt-menu__item ${pathname === '/customers' ? 'kt-menu__item--active' : '' } `} aria-haspopup="true"><Link to='/customers'
         className="kt-menu__link"><span className="kt-menu__link-icon">
               <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" className="kt-svg-icon">
                 <g stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
@@ -26,24 +26,15 @@ const sidebar=(props)=> {
                   <path d="M11.0563554,18.6706981 L5.33593024,14.122919 C4.94553994,13.8125559 4.37746707,13.8774308 4.06710397,14.2678211 C4.06471678,14.2708238 4.06234874,14.2738418 4.06,14.2768747 L4.06,14.2768747 C3.75257288,14.6738539 3.82516916,15.244888 4.22214834,15.5523151 C4.22358765,15.5534297 4.2250303,15.55454 4.22647627,15.555646 L11.0872776,20.8031356 C11.6250734,21.2144692 12.371757,21.2145375 12.909628,20.8033023 L19.7677785,15.559828 C20.1693192,15.2528257 20.2459576,14.6784381 19.9389553,14.2768974 C19.9376429,14.2751809 19.9363245,14.2734691 19.935,14.2717619 L19.935,14.2717619 C19.6266937,13.8743807 19.0546209,13.8021712 18.6572397,14.1104775 C18.654352,14.112718 18.6514778,14.1149757 18.6486172,14.1172508 L12.9235044,18.6705218 C12.377022,19.1051477 11.6029199,19.1052208 11.0563554,18.6706981 Z" id="Path" fill="#000000" opacity="0.3" />
                 </g>
               </svg></span><span className="kt-menu__link-text list-style">Dashboard</span></Link></li>
-        <li className={`kt-menu__item ${pathname === '/inbox' ? 'kt-menu__item--active' : '' } `}  aria-haspopup="true"><Link to='/inbox' className="kt-menu__link "><span className="kt-menu__link-icon">
+        <li className={`kt-menu__item ${pathname === '/addCustomers' ? 'kt-menu__item--active' : '' } `}  aria-haspopup="true"><Link to='/addCustomers' className="kt-menu__link "><span className="kt-menu__link-icon">
               <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" className="kt-svg-icon">
                 <g stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
                   <rect id="bound" x={0} y={0} width={24} height={24} />
                   <path d="M6,2 L18,2 C18.5522847,2 19,2.44771525 19,3 L19,12 C19,12.5522847 18.5522847,13 18,13 L6,13 C5.44771525,13 5,12.5522847 5,12 L5,3 C5,2.44771525 5.44771525,2 6,2 Z M7.5,5 C7.22385763,5 7,5.22385763 7,5.5 C7,5.77614237 7.22385763,6 7.5,6 L13.5,6 C13.7761424,6 14,5.77614237 14,5.5 C14,5.22385763 13.7761424,5 13.5,5 L7.5,5 Z M7.5,7 C7.22385763,7 7,7.22385763 7,7.5 C7,7.77614237 7.22385763,8 7.5,8 L10.5,8 C10.7761424,8 11,7.77614237 11,7.5 C11,7.22385763 10.7761424,7 10.5,7 L7.5,7 Z" id="Combined-Shape" fill="#000000" opacity="0.3" />
                   <path d="M3.79274528,6.57253826 L12,12.5 L20.2072547,6.57253826 C20.4311176,6.4108595 20.7436609,6.46126971 20.9053396,6.68513259 C20.9668779,6.77033951 21,6.87277228 21,6.97787787 L21,17 C21,18.1045695 20.1045695,19 19,19 L5,19 C3.8954305,19 3,18.1045695 3,17 L3,6.97787787 C3,6.70173549 3.22385763,6.47787787 3.5,6.47787787 C3.60510559,6.47787787 3.70753836,6.51099993 3.79274528,6.57253826 Z" id="Combined-Shape" fill="#000000" />
                 </g>
-              </svg></span><span className="kt-menu__link-text  list-style">Inbox</span></Link></li>
-        <li className={`kt-menu__item ${pathname === '/automate' ? 'kt-menu__item--active' : '' } `}  aria-haspopup="true"><Link to='/automate' className="kt-menu__link "><span className="kt-menu__link-icon">
-              <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" className="kt-svg-icon">
-                <g stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
-                  <rect id="bound" x={0} y={0} width={24} height={24} />
-                  <path d="M12,8 L8,8 C5.790861,8 4,9.790861 4,12 L4,13 C4,14.6568542 5.34314575,16 7,16 L7,18 C4.23857625,18 2,15.7614237 2,13 L2,12 C2,8.6862915 4.6862915,6 8,6 L12,6 L12,4.72799742 C12,4.62015048 12.0348702,4.51519416 12.0994077,4.42878885 C12.264656,4.2075478 12.5779675,4.16215674 12.7992086,4.32740507 L15.656242,6.46136716 C15.6951359,6.49041758 15.7295917,6.52497737 15.7585249,6.56395854 C15.9231063,6.78569617 15.876772,7.09886961 15.6550344,7.263451 L12.798001,9.3840407 C12.7118152,9.44801079 12.607332,9.48254921 12.5,9.48254921 C12.2238576,9.48254921 12,9.25869158 12,8.98254921 L12,8 Z" id="Combined-Shape" fill="#000000" />
-                  <path d="M12.0583175,16 L16,16 C18.209139,16 20,14.209139 20,12 L20,11 C20,9.34314575 18.6568542,8 17,8 L17,6 C19.7614237,6 22,8.23857625 22,11 L22,12 C22,15.3137085 19.3137085,18 16,18 L12.0583175,18 L12.0583175,18.9825492 C12.0583175,19.2586916 11.8344599,19.4825492 11.5583175,19.4825492 C11.4509855,19.4825492 11.3465023,19.4480108 11.2603165,19.3840407 L8.40328311,17.263451 C8.18154548,17.0988696 8.13521119,16.7856962 8.29979258,16.5639585 C8.32872576,16.5249774 8.36318164,16.4904176 8.40207551,16.4613672 L11.2591089,14.3274051 C11.48035,14.1621567 11.7936615,14.2075478 11.9589099,14.4287888 C12.0234473,14.5151942 12.0583175,14.6201505 12.0583175,14.7279974 L12.0583175,16 Z" id="Combined-Shape" fill="#000000" opacity="0.3" />
-                </g>
-              </svg></span><span className="kt-menu__link-text  list-style">Automate</span></Link></li>
-        <li className={`kt-menu__item ${(pathname === '/account' || pathname === '/team' || pathname === '/profile'  || pathname === '/team'  || pathname === '/billing'  || pathname === '/social_account' || pathname === '/referrals'  || pathname === '/support' ) ? 'kt-menu__item--active' : '' } `}  aria-haspopup="true"><Link to='/account' className="kt-menu__link "><span className="kt-menu__link-icon">
-              <i className="flaticon-avatar" /></span><span className="kt-menu__link-text  list-style">Account</span></Link></li>
+              </svg></span><span className="kt-menu__link-text  list-style">Add Customer</span></Link></li>
+       
       </ul>
       <div className="ps__rail-x" style={{left: '0px', bottom: '0px'}}><div className="ps__thumb-x" tabIndex={0} style={{left: '0px', width: '0px'}} /></div><div className="ps__rail-y" style={{top: '0px', right: '3px'}}><div className="ps__thumb-y" tabIndex={0} style={{top: '0px', height: '0px'}} /></div></div>
   </div>
